b7ddd580249a93f1a702e029a704646a06d425c0
Ronaldo Milfont <milfont@lesc.ufc.br>
2016-10-26 11:13:24 -0300

    Make output arbiter more generic
    Now outputArbiter.vhd has no reference to ThorPackage and are completely
    generic.
    
    Commit:Ref
    
    TaskNumber:#41

--- Teste/NoC/outputArbiter.vhd
+++ Teste/NoC/outputArbiter.vhd
@@ -1,29 +1,30 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.all;
-use work.ThorPackage.all;
 
-entity outputArbiter is    
-port(
-    freePort :              in regNport;
-    enable :                in std_logic;
-    enablePort :            in regNport;
-    isOutputSelected :      out std_logic;
-    selectedOutput :        out integer
-);
+entity outputArbiter is   
+    generic(    
+        size : natural 
+    );
+    port(
+        requests :              in std_logic_vector(size-1 downto 0);
+        enable :                in std_logic;
+        isOutputSelected :      out std_logic;
+        selectedOutput :        out integer
+    );
 end;
 
 architecture outputArbiter of outputArbiter is
 begin
     
-    process(freePort, enablePort, enable)
+    process(requests, enable)
         variable auxDone : std_logic;
         variable auxSelected : integer;
     begin
         auxDone := '0';
         auxSelected := 0;
         if(enable = '1') then
-            for i in EAST to LOCAL-1 loop
-                if(enablePort(i) = '1' and freePort(i) = '1') then
+            for i in requests'low to requests'high loop
+                if requests(i) = '1' then
                     auxSelected := i;
                     auxDone := '1';
                     exit;
