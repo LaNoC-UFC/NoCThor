997541ed12dfa56278f00a79166bbc166961977f
Rafael Mota <rafaelmota@lesc.ufc.br>
2016-11-01 11:33:26 -0300

    Fixing network size in 2x2
    Commit:Mod
    
    TaskNumber:#54

--- Teste/Monitores/SC_common.h
+++ Teste/Monitores/SC_common.h
@@ -2,8 +2,8 @@
 #define SC_COMMON_MOD
 
 #define TAM_FLIT 16
-#define NUM_ROT_X 11
-#define NUM_ROT_Y 11
+#define NUM_ROT_X 2
+#define NUM_ROT_Y 2
 #define NUM_EP NUM_ROT_X*NUM_ROT_Y
 
 #include "systemc.h"
